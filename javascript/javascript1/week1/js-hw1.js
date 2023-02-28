08429b03551a071c00c11cecdffcc39e1b37bfa3
Arash <arash8877@gmail.com>
2023-02-28 20:53:34 +0100

    Age-ify task

---
+++ javascript/javascript1/week1/js-hw1.js
@@ -0,0 +1,7 @@
+// my user-name in freeCodeCamp.com is "arash8877".
+
+// Age-ify
+const yearOfBirth = 1979;
+const yearFuture = 2027;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}.`);
