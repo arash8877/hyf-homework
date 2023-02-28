9284c25991e621e068113929c2e1e85c7592d230
Arash <arash8877@gmail.com>
2023-02-28 20:54:41 +0100

    Goodboy-Oldboy task

--- javascript/javascript1/week1/js-hw1.js
+++ javascript/javascript1/week1/js-hw1.js
@@ -5,3 +5,19 @@ const yearOfBirth = 1979;
 const yearFuture = 2027;
 const age = yearFuture - yearOfBirth;
 console.log(`You will be ${age} years old in ${yearFuture}.`);
+
+// Goodboy-Oldboy
+const dogYearOfBirth = 2020;
+const dogYearFuture = 2045;
+const dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears) {
+  console.log(
+    `Your dog will be ${dogYear * 7} dog years old in ${dogYearFuture}`
+  );
+} else {
+  console.log(
+    `Your dog will be ${dogYear} human years old in ${dogYearFuture}`
+  );
+}
