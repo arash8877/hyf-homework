5fa326c5029a9e2c0bb892844defdb2a0df59c13
Arash <arash8877@gmail.com>
2023-07-02 18:28:38 +0200

    apllied the feedback

--- react/react1/week1/todo_list/src/components/ListItem.js
+++ react/react1/week1/todo_list/src/components/ListItem.js
@@ -3,13 +3,9 @@ import "./ListItem.css";
 
 const ListItem = ({ todo, time, id }) => {
   return (
-    <div>
       <li>
-        <h3>
-          {id}: {todo}: {time}
-        </h3>
+          {id}- {todo}: {time}
       </li>
-    </div>
   );
 };
 
